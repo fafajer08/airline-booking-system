4324d6b5e9a433a33630062f9c5a2f1dd0385b2e
jaydescalsota <jaydescalsota@gmail.com>
2024-08-30 17:32:55 +0800

    add search flight page

---
+++ client/src/pages/FlightOptions.js
@@ -0,0 +1,13 @@
+// import Banner from "../components/Banner";
+// import Highlights from "../components/Highlights";
+// import Course from "../components/Course"
+
+
+export default function FlightOptions() {
+  return (
+    <div>
+        <h1>Flight Options Page</h1>
+
+    </div>
+  );
+}
