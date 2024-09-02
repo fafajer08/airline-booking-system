f67cfb6d36de91b355106e10a8723a6943a7e922
jaydescalsota <jaydescalsota@gmail.com>
2024-09-02 21:03:26 +0800

    add flight table components

--- client/src/pages/FlightOptions.js
+++ client/src/pages/FlightOptions.js
@@ -1,23 +1,21 @@
-// import Banner from "../components/Banner";
-// import Highlights from "../components/Highlights";
-// import Course from "../components/Course"
 import React from 'react';
 import NavBar from "../components/NavBar";
 import Carousel from "../components/Carousel";
 import { BackButton, ContinueButton } from '../components/Buttons';
-
+import FlightTable from "../components/FlightTable";
 
 export default function FlightOptions() {
   return (
     <div>
-    <NavBar />
+      <NavBar />
       <div className="container">
-        <h5 className="mt-5 ms-5 px-5" > Select you flight</h5>
+        <h5 className="mt-5 ms-5 px-5">Select your flight</h5>
         <h2 className="ms-5 px-5">Davao DVO bound for Manila MNL</h2>
         <Carousel />
-        <div>
+        <FlightTable />
+        <div className="button-container m-4">
           <BackButton link="http://localhost:3000/searchflight" />
-          <ContinueButton link="http://localhost:3000/guestdetails" />
+          <ContinueButton  link="http://localhost:3000/guestdetails" />
         </div>
       </div>
     </div>
