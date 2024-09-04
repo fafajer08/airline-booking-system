1fd13aa9c348318f098e3a295e78cde406bb341b
jaydescalsota <jaydescalsota@gmail.com>
2024-09-04 20:00:35 +0800

    add admin page and components

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -12,12 +12,51 @@ export default function Payment() {
     <div>
     <NavBar />
       <div className="container">
-        <h5 className="mt-5 ms-5 px-5" > How would you like to pay?</h5>
-        <h2 className="ms-5 px-5">Payment Method</h2>
+        <h5 className="" > How would you like to pay?</h5>
+        <h2 className="">Payment Method</h2>
+        <div className='px-5 py-3'>
+              <div>
+                  <h3>Credit or Debit Card</h3>
+              </div>
+              <div className=''>
+                  <input className='' type="radio" name="paymentMethod" id="creditCard" value="alipay" />
+                  <label className='px-5 py-3'for="creditCard">Credit, Debit or Prepaid Cards</label>
+              </div>
 
+        </div>
+
+
+        <div className='px-5 py-3'>
+              <div>
+                <h3>E-Wallet</h3>
+              </div>
+              <div className=''>
+                  <input className='' type="radio" name="paymentMethod" id="creditCard" value="alipay" />
+                  <label className='px-5 py-3'for="creditCard">Alipay</label>
+              </div>
+              <div>
+                  <input type="radio" name="paymentMethod" id="creditCard" value="applepay" />
+                  <label className='px-5 py-3' for="creditCard">Apple Pay</label>
+              </div>
+              <div>
+                  <input type="radio" name="paymentMethod" id="creditCard" value="googlepay" />
+                  <label className='px-5 py-3' for="creditCard">Google Pay</label>
+              </div>
+              <div>
+                  <input type="radio" name="paymentMethod" id="creditCard" value="paypal" />
+                  <label className='px-5 py-3' for="creditCard">Paypal</label>
+              </div>
+        </div>
+        
         <div>
-          <BackButton link="http://localhost:3000/bookingsummary" />
-          <SubmitButton />
+          <p>By click ‘Continue’ button below, I confirm that I have read, understood, and accept all the Conditions set by the airline. </p>
+        </div>
+
+        <div className='d-flex '>
+            <div className='ms-auto'>
+              <BackButton link="http://localhost:3000/guestdetails" />
+              <SubmitButton />
+            </div>
         </div>
       </div>
     </div>
