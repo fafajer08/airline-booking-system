8bba59b90249edbe76a92eb8618d39b63fdeff13
jaydescalsota <jaydescalsota@gmail.com>
2024-09-24 16:22:14 +0800

    disable session for OAuth

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 // Define environment config
 const port = process.env.PORT || 3000;
 const mongodb = process.env.MONGODB_STRING;
-const secret = process.env.clientSecret;
+// const secret = process.env.clientSecret;
 const frontend = process.env.FRONTEND;
 
 // Setup middleware
@@ -36,15 +36,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
 // Setup session and passport
-app.use(session({
-    secret: secret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false, // Set to true if you're using HTTPS in production
-        maxAge: 24 * 60 * 60 * 1000 // 24 hours session expiry
-    }
-}));
+// app.use(session({
+//     secret: secret,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         secure: false, // Set to true if you're using HTTPS in production
+//         maxAge: 24 * 60 * 60 * 1000 // 24 hours session expiry
+//     }
+// }));
 // app.use(passport.initialize());
 // app.use(passport.session());
 
